77fdb9c536d061cc1fa1342652bee2b93c5a1f75
AryanNanda <65027409+AryanNanda@users.noreply.github.com>
2021-10-08 21:41:43 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase' 
+require('@firebase/firestore')
+
+ export const firebaseConfig = {
+    apiKey: "AIzaSyBl5-4-djGkHVBCO9oIilG5Wv-qWtz4Rok",
+    authDomain: "c-85-3a0a7.firebaseapp.com",
+    projectId: "c-85-3a0a7",
+    storageBucket: "c-85-3a0a7.appspot.com",
+    messagingSenderId: "523965081928",
+    appId: "1:523965081928:web:0a2cc6c88cc02cb1c32b7d"
+  };
